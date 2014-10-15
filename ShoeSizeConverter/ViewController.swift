d0f43b8b9b471f85947b14a691cf091746931306
sfhome1 <sfhome1@icloud.com>
2014-10-15 16:55:08 -0700

    second rev changes Women Shoe Size

--- ShoeSizeConverter/ViewController.swift
+++ ShoeSizeConverter/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mensShoeSizeText: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
 
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+/*       refactoring coding to reduce number of line of code
+
+         let sizeFromTextField = mensShoeSizeText.text
+         let numberFromTextField =    sizeFromTextField.toInt()
+          var integerFromTextField = numberFromTextField!
+*/
+        
+        let sizeFromTextField = mensShoeSizeText.text.toInt()!
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false
+
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " In European Shoe Size"
+        
+          }
+
+        
+        
+        
+        
+    @IBAction func womenConvertButtonPressed(sender: AnyObject) {
+   
+    
+    
+    }
+        
 
 }
 
